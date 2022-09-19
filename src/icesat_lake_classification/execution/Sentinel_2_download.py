bf501d32cec0d662d5dcd097310425fe05c49a1e
gjvanleeuwen <g.j.vanleeuwens@gmail.com>
2022-09-19 14:17:06 +0200

    add script to download S2 data

---
+++ src/icesat_lake_classification/execution/Sentinel_2_download.py
@@ -0,0 +1,35 @@
+# connect to the API
+from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
+from datetime import date
+import tenacity
+
+@tenacity.retry(stop=tenacity.stop_after_attempt(3), wait=tenacity.wait_fixed(3600))
+def download_all(products, outpath):
+    return api.download_all(products, directory_path=outpath)
+
+
+
+if __name__ == "__main__":
+    api = SentinelAPI('ertyboi', 'Sentinel1!', 'https://scihub.copernicus.eu/dhus')
+
+    outpath = "F:/onderzoeken/thesis_msc/data/Sentinel/20190620"
+    geometry = {"type":"Polygon","coordinates":[[[-49.509888,69.597805],[-47.488403,69.553715],[-47.471924,67.32716],[-48.076172,64.031339],[-49.559326,63.821288],[-49.509888,69.597805]]]}
+
+    # search by polygon, time, and SciHub query keywords
+    footprint = geojson_to_wkt(geometry)
+    products = api.query(footprint,
+                         date=('20190619', date(2019, 6, 20)),
+                         platformname='Sentinel-2',
+                         cloudcoverpercentage=(0, 30),
+                         producttype='S2MSI2A')
+
+    for product_id in products.keys():
+        is_online = api.is_online(product_id)
+
+        if is_online:
+            print('Product {} is online. Starting download.'.format(product_id))
+        else:
+            print('Product {} is not online.'.format(product_id))
+
+    downloaded, triggered, failed = download_all(products,outpath=outpath)
+
